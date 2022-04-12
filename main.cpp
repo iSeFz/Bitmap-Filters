3823df8bdd136eeafbe2d6106e1ad85485621f0b
SeifYahia <seifyahia@gmail.com>
2022-04-12 15:49:42 +0200

    Added shrink filter and other small edits

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 /*
-FCAI – Programming 1 – 2022 - Assignment 3 - Part 1
+FCAI – Programming 1 – 2022 - Assignment 3 - Part 2
 Group: S13
 Author1 and ID: Seif Yahia - 20210172
 Author2 and ID: Mohamed Amgad - 20210330
 Author3 and ID: Omar Elgammal - 20210271
 Program Name: Bitmap Filters
-Last Modification Date: 7 April 2022
-Version: 1.8
+Last Modification Date: 12 April 2022
+Version: 2.0
 */
 
 // Including necessary libraries
@@ -25,12 +25,13 @@ unsigned char image[SIZE][SIZE];
 // Functions prototype
 void load_image();
 void save_image();
-void merge_image();
-void darken_lighten();
-void invert_image();
-void rotate_image();
 void black_white();
+void invert_image();
+void merge_image();
 void flip_image();
+void darken_lighten();
+void rotate_image();
+void shrink_image();
 
 // Main function to start the program
 int main(){
@@ -41,10 +42,11 @@ int main(){
     load_image();
 
     // Output the menu to the user
-    cout << "The program can apply the following filters to bitmap greyscale images:\n"
-            "1. Merge Two Images\n2. Darken or Lighten Image\n"
-            "3. Invert Image\n4. Rotate Image\n"
-            "5. Black and white filter\n6. Horizontal or Vertical flip\n"
+    cout << "The program can apply the following filters to bitmap grayscale images:\n"
+            "1. Black & White\n2. Invert Image\n"
+            "3. Merge Two Images\n4. Flip Image\n"
+            "5. Darken or Lighten Image\n6. Rotate Image\n"
+            "9. Shrink Image\n"
             "s. Save Image to a new file\n0. Exit\n";
 
     // Check the input of the user and reject bad inputs
@@ -52,22 +54,25 @@ int main(){
         cout << "Choose one of the above filters to apply or 0 to EXIT: ";
         cin >> choice;
         if(choice == "1"){
-            merge_image();
-        }
-        else if(choice == "2"){
-            darken_lighten();
-        }
-        else if(choice == "3"){
-            invert_image();
-        }
-        else if(choice == "4"){
-            rotate_image();
-        }
-        else if(choice == "5"){
             black_white();
         }
-        else if(choice == "6"){
+        else if(choice == "2"){
+            invert_image();
+        }
+        else if(choice == "3"){
+            merge_image();
+        }
+        else if(choice == "4"){
             flip_image();
+        }
+        else if(choice == "5"){
+            darken_lighten();
+        }
+        else if(choice == "6"){
+            rotate_image();
+        }
+        else if(choice == "9"){
+            shrink_image();
         }
         else if(choice == "s"){
             save_image();
@@ -93,8 +98,8 @@ int main(){
 // Load file name and contents of the image
 void load_image(){
     char imageFileName[100];
-    // Get greyscale file name
-    cout << "Please enter the greyscale source image file name: ";
+    // Get grayscale file name
+    cout << "Please enter the grayscale source image file name: ";
     cin >> imageFileName;
     // Concatenate the extension of bitmap with file name
     strcat(imageFileName, ".bmp");
@@ -112,6 +117,48 @@ void save_image(){
     strcat(imageFileName, ".bmp");
     // Write contents of the bitmap into a new file
     writeGSBMP(imageFileName, image);
+    // Output a message to the user to confirm the saving proccess
+    cout << "Image was saved successfully as \"" << imageFileName << "\"\n";
+}
+
+// Convert image into black and white image
+void black_white(){
+    long avg = 0;
+    // Iterate on each pixel to collect the sum of pixels values
+    for (int i = 0; i < SIZE; ++i){
+        for (int j = 0; j < SIZE; ++j){
+            avg += image[i][j];
+        }
+    }
+
+    // Get average of values of pixels
+    avg /= (SIZE * SIZE);
+
+    // Turn image into black and white
+    for (int i = 0; i < SIZE; ++i){
+        for (int j = 0; j < SIZE; ++j){
+            // Pixel that has value greater than avg turn into white
+            if (image[i][j] > avg){
+                image[i][j] = 255;
+            }
+            // Pixel that has value smaller than avg turn into black
+            else{
+                image[i][j] = 0;
+            }
+        }
+    }
+    cout << "Black & white filter applied to the image successfully!\n";
+}
+
+// Function that inverts image colour
+void invert_image(){
+    // Iterates on each pixel and inverts its color
+    for(int i = 0; i < SIZE; ++i){
+        for(int j = 0; j < SIZE; ++j){
+            image[i][j] = 255 - image[i][j];
+        }
+    }
+    cout << "Invert filter was applied successfully!\n";
 }
 
 // Merge filter to merge two photos together
@@ -139,6 +186,36 @@ void merge_image(){
     cout << "Two images merged successfully!\n";
 }
 
+// Flip image horizontally or vertically
+void flip_image(){
+    string choice;
+    cout << "Do you want to flip (H)orizontally or (V)ertically? ";
+    cin >> choice;
+
+    if(choice == "V"){
+        // Iterate on half number of pixels and swap them vertically
+        for(int i = 0; i < SIZE / 2; ++i){
+            for (int j = 0; j < SIZE; ++j){
+                swap(image[i][j], image[(SIZE - 1) - i][j]);
+            }
+        }
+        cout << "Image flipped vertically!\n";
+    }
+    else if(choice == "H"){
+        // Iterate on half number of pixels and swap them horizontally
+        for(int i = 0; i < SIZE; ++i){
+            for (int j = 0; j < SIZE / 2; ++j){
+                swap(image[i][j], image[i][(SIZE - 1) - j]);
+            }
+        }
+        cout << "Image flipped horizontally!\n";
+    }
+    else{
+        cout << "Enter ONlY \"H\" or \"V\"\n";
+        flip_image();
+    }
+}
+
 // Darken or Lighten the image by 50%
 void darken_lighten(){
     string choice;
@@ -151,8 +228,7 @@ void darken_lighten(){
         filter = 0;
         cout << "Darken filter was applied successfully!\n";
     }
-
-        // Lighten the image by merging it with a white image
+    // Lighten the image by merging it with a white image
     else if(choice == "L"){
         filter = 255;
         cout << "Lighten filter was applied successfully!\n";
@@ -168,18 +244,6 @@ void darken_lighten(){
             image[i][j] = (image[i][j] + filter) / 2;
         }
     }
-}
-
-// Function that inverts image colour
-void invert_image(){
-
-    // Iterates on each pixel and inverts its colour
-    for(int i = 0; i < SIZE; ++i){
-        for(int j = 0; j < SIZE; ++j){
-            image[i][j] = 255 - image[i][j];
-        }
-    }
-    cout << "Invert Image filter was applied successfully!\n";
 }
 
 // Function that rotates image anticlockwise
@@ -229,69 +293,40 @@ void rotate_image(){
         }
         nRotations--;
     }
-    cout << "Image rotated successfully!\n";
+    cout << "Image rotated by " << input << " successfully!\n";
 }
 
-// Convert image into black and white image
-void black_white(){
-    long avg = 0;
-
-    // Iterate on each pixel to collect the sum of pixels values
-    for (int i = 0; i < SIZE; ++i){
-        for (int j = 0; j < SIZE; ++j){
-            avg += image[i][j];
-        }
-    }
-
-    // Get average of values of pixels
-    avg /= (SIZE * SIZE);
-
-    // Turn image into black and white
-    for (int i = 0; i < SIZE; ++i){
-        for (int j = 0; j < SIZE; ++j){
-
-            // Pixel that has value greater than avg turn into white
-            if (image[i][j] > avg){
-                image[i][j] = 255;
-            }
-
-                // Pixel that has value smaller than avg turn into black
-            else{
-                image[i][j] = 0;
-            }
-        }
-    }
-    cout << "Black and white filter applied to the image successfully!\n";
-}
-
-// Flip image horizontally or vertically
-void flip_image(){
+// Shrink filter to shrink image to 1/2, 1/3 or 1/4
+void shrink_image(){
     string choice;
-    cout << "Do you want to flip (H)orizontally or (V)ertically? ";
+    int amount;
+    unsigned char temp;
+    cout << "Shrink to (1/2), (1/3) or (1/4)? ";
     cin >> choice;
-
-    if(choice == "V"){
-
-        // Iterate on half number of pixels and swap them vertically
-        for(int i = 0; i < SIZE / 2; ++i){
-            for (int j = 0; j < SIZE; ++j){
-                swap(image[i][j], image[(SIZE - 1) - i][j]);
-            }
-        }
-        cout << "Image flipped vertically!\n";
+    // Check the inputs of the user and reject bad inputs
+    if(choice == "1/2"){
+        amount = 2;
+        cout << "Image shrinked to 1/2 successfully!\n";
     }
-    else if(choice == "H"){
-
-        // Iterate on half number of pixels and swap them horizontally
-        for(int i = 0; i < SIZE; ++i){
-            for (int j = 0; j < SIZE / 2; ++j){
-                swap(image[i][j], image[i][(SIZE - 1) - j]);
-            }
-        }
-        cout << "Image flipped horizontally!\n";
+    else if(choice == "1/3"){
+        amount = 3;
+        cout << "Image shrinked to 1/3 successfully!\n";
+    }
+    else if(choice == "1/4"){
+        amount = 4;
+        cout << "Image shrinked to 1/4 successfully!\n";
     }
     else{
-        cout << "Enter ONlY \"H\" or \"V\"\n";
-        flip_image();
+        cout << "INVALID INPUT!!\n";
+        shrink_image();
+    }
+    // Iterate over every pixel and write the new pixels
+    // in the new dimensions according to the amount of shrink
+    for(int i = 0; i < SIZE; ++i){
+        for(int j = 0; j < SIZE; ++j){
+            temp = image[i][j];
+            image[i][j] = 255;
+            image[i / amount][j / amount] = temp;
+        }
     }
 }
