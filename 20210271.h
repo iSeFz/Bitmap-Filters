e3d3d36d3ec0ca691e29b76a63000815a366fd18
Omar <omarelgammal2004@gmail.com>
2022-04-18 02:57:40 +0200

    Added shuffle image filter and some changes in enlarge image filter

--- 20210271.h
+++ 20210271.h
@@ -85,26 +85,26 @@ void enlarge_image(){
 
     // Loop that takes input from user 
     // and sets up the iterator variables accordinng to user's choice
-    int i_start, i_end, j_start, j_end;
+    int i_str_temp, j_str_temp;
     while(!correct){
-        cout << "Please choose which quarter you would like to enlarge (1, 2, 3, 4): ";
+        cout << "Please choose which quarter you would like to enlarge (1, 2, 3, 4):";
         cin >> input;
         
         if (input == "1"){
-            i_start = 0, i_end = 128, 
-            j_start = 0,  j_end = 128;
+            i_str_temp = 0, 
+            j_str_temp = 0;
         }
         else if (input == "2"){
-            i_start = 0, i_end = 128, 
-            j_start = 128,  j_end = 256;
+            i_str_temp = 0, 
+            j_str_temp = 128;
         }
         else if (input == "3"){
-            i_start = 128, i_end = 256, 
-            j_start = 0,  j_end = 128;
+            i_str_temp = 128, 
+            j_str_temp = 0;
         }
         else if (input == "4"){
-            i_start = 128, i_end = 256, 
-            j_start = 128,  j_end = 256;
+            i_str_temp = 128, 
+            j_str_temp = 128;
         }
         else{
             cout << "Invalid input! Please enter a number from 1 to 4.\n";
@@ -114,12 +114,103 @@ void enlarge_image(){
     }
     
     // Copying the quarter from the temporary image to the original image but enlarged
-    for(int i=0, i_quarter = i_start; i < SIZE; i += 2, i_quarter++){
-        for(int j=0, j_quarter = j_start; j < SIZE; j += 2, j_quarter++){
+    for(int i=0, i_quarter = i_str_temp; i < SIZE; i += 2, i_quarter++){
+        for(int j=0, j_quarter = j_str_temp; j < SIZE; j += 2, j_quarter++){
             image[i][j] = image[i + 1][j] = image[i][j + 1] 
             = image[i + 1][j + 1] = temp[i_quarter][j_quarter];
         }
     }
 
     cout << "Image enlarged successfully!\n";
+}
+
+// Filter #b
+// Function that shuffles image quraters according to the user's input
+void shuffle_image(){
+
+    // initializing temporary image array
+    unsigned char temp[SIZE][SIZE];
+
+    // Copying original image to temporary array
+    for (int i = 0; i < SIZE; i++){
+            for (int j = 0; j < SIZE; j++){
+                temp[i][j] = image[i][j];
+            }
+        }
+
+    // Initializing user input variables
+    string input[4];
+    int correct = 0;
+
+    // Loop that takes input from user 
+    // and checks if input is correct
+    int i_str_temp, j_str_temp, i_str_original, j_str_original;
+    while(!correct){
+        cout << "Please enter the order of quarters in the new image\n(4 space separated numbers from 1 to 4):";
+        for (int i = 0; i < 4; i++){
+            cin >> input[i];
+        }    
+        correct = 1;
+        
+        for(int i = 0; i < 4; i++){
+            if(!(input[i] == "1" || input[i] == "2" || input[i] == "3" || input[i] == "4")){
+                correct = 0;
+            }
+        }
+        
+        
+        if(!correct){
+            cout << "Invalid input! Input example:4 2 1 3\n";
+        }
+    }
+
+    // Loop that sets the iterator variables according to the user's input in the temp image 
+    // and copy it into the original image quarter
+    for(int i = 0; i < 4; i++){
+
+        // Conditions to set up the iterator variables on temporary image
+        if (input[i] == "1"){
+            i_str_temp = 0, 
+            j_str_temp = 0;
+        }
+        else if (input[i] == "2"){
+            i_str_temp = 0,
+            j_str_temp = 128;
+        }
+        else if (input[i] == "3"){
+            i_str_temp = 128,
+            j_str_temp = 0;
+        }
+        else if (input[i] == "4"){
+            i_str_temp = 128,
+            j_str_temp = 128;
+        }
+
+        // Conditions to set up the iterator variables for the original image
+        if (i == 0){
+            i_str_original = 0, 
+            j_str_original = 0;
+        }
+        else if (i == 1){
+            i_str_original = 0,
+            j_str_original = 128;
+        }
+        else if (i == 2){
+            i_str_original = 128,
+            j_str_original = 0;
+        }
+        else if (i == 3){
+            i_str_original = 128,
+            j_str_original = 128;
+        }
+
+        // loop that iterates over both quarters and copies from temporary to original image
+        for(int i_origin = i_str_original, i_temp = i_str_temp; i_origin < i_str_original + 128; i_origin++, i_temp++){
+            for(int j_origin = j_str_original, j_temp = j_str_temp; j_origin < j_str_original + 128; j_origin++, j_temp++){
+                image[i_origin][j_origin] = temp[i_temp][j_temp];
+
+            }
+        }
+    }
+    cout << "Image shuffled successfully!\n";
 }
