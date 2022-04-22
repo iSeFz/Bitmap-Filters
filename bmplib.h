63869985b6a349bd0341d136d5affc6c2036ae80
SeifYahia <seifyahia@gmail.com>
2022-04-22 03:57:30 +0200

    Removed unnecessary result array

--- bmplib.h
+++ bmplib.h
@@ -10,9 +10,6 @@ unsigned char image[SIZE][SIZE];
 // Declaring a global 3D array for image
 unsigned char imageRGB[SIZE][SIZE][RGB];
 
-// Declaring a global 3D array for some filters
-unsigned char result[SIZE][SIZE][RGB];
-
 // read full-color image from the file specified by filename, into inputImage
 int readRGBBMP(const char* filename, unsigned char inputImage[][SIZE][RGB]);
 
@@ -20,7 +17,7 @@ int readRGBBMP(const char* filename, unsigned char inputImage[][SIZE][RGB]);
 int writeRGBBMP(const char* filename, unsigned char outputImage[][SIZE][RGB]);
 
 // read grayscale image from the file specified by filename, into inputImage
-int readGSBMP(const char* filename, unsigned char image[][SIZE]);
+int readGSBMP(const char* filename, unsigned char inputImage[][SIZE]);
 
 // write grayscale image to the file specified by filename, from outputImage
 int writeGSBMP(const char* filename, unsigned char outputImage[][SIZE]);
